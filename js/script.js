b522ae9e120abd1ad11ea1591354548dc4033c0c
Mainazarov Islambek <56814948+mainazarov1@users.noreply.github.com>
2019-11-07 01:02:31 +0600

    Revert "Adaptive"

--- js/script.js
+++ js/script.js
@@ -3,39 +3,3 @@ $('.burger-menu').on('click', function (e) {
     $('.burger-menu').toggleClass('burger-open');
     $('.menu').toggleClass('menu-show')
 });
-
-$(document).ready(function(){
-    $(".projects-slider").owlCarousel( {
-        items: 2,
-        loop: true,
-        nav: true,
-        dots: false,
-        navText: ['<svg width="23px" height="23px" viewBox="0 0 11 20"><path style="fill:none;stroke-width:2px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="23px" height="23px" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 2px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-        responsive:{
-            0:{
-                items: 1
-            },
-            400:{
-                items: 1
-            },
-            576:{
-                items: 1
-            },
-            768:{
-                items: 1
-            },
-            992:{
-                items: 2
-            }
-        }
-    });
-});
-$(document).ready(function(){
-    $(".rent-slider").owlCarousel( {
-        items: 1,
-        loop: true,
-        nav: true,
-        dots: false,
-        navText: ['<svg width="23px" height="23px" viewBox="0 0 11 20"><path style="fill:none;stroke-width:2px;stroke: #005BC1;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="23px" height="23px" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 2px;stroke: #005BC1;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
-    });
-});
